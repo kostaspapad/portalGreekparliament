108b98875f4f00ccb76e0e5f34bae906e70b2a4e
SPIROS\spiros <uberi12345@hotmail.com>
2018-06-27 00:44:55 +0300

    test

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:api')->get('speakers', function(){
+Route::get('speakers', function(){
     $speakers = Speaker::orderBy('id','desc')->paginate(51);
     echo "Eeeee";die;
     return response()->json(compact('speakers'));
