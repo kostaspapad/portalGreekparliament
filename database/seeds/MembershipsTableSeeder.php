17c8659125e2bc50c9daa10c71ea637ff380514c
kospa <kospa@protonmai.com>
2018-07-10 18:11:47 +0300

    Fix start date, end date in Memberships Seeder

--- database/seeds/MembershipsTableSeeder.php
+++ database/seeds/MembershipsTableSeeder.php
@@ -13,10 +13,16 @@ class MembershipsTableSeeder extends Seeder
     public function run()
     {
         $counter = 1;
-        DB::table('memberships')->delete();
-        $json = File::get("database/data/memberships.json");
-        $data = json_decode($json);
+        DB::table('memberships')->truncate();
+        $memberships_json = File::get("database/data/memberships.json");
+        $events_json = File::get("database/data/events.json");
+
+        $data = json_decode($memberships_json);
+        $events = json_decode($events_json);
+
         foreach ($data as $obj) {
+            // print_r($obj);
+            
             // Both dates exist
             if(isset($obj->start_date) && isset($obj->end_date)){
                 Membership::create(array(
@@ -33,6 +39,18 @@ class MembershipsTableSeeder extends Seeder
 
             // End date does not exist
             else if(isset($obj->start_date) && !isset($obj->end_date)){
+                $obj->end_date = NULL;
+                // When end date does not exist get end date from json events file
+                // join event id with legislative_period_id and if exist get en date
+                // If the legislative period has not ended end_date = null
+                foreach($events as $ev){
+                    if($ev->classification == "legislative period" && $ev->id == $obj->legislative_period_id){
+                        if(isset($ev->end_date)){
+                            $obj->end_date = $ev->end_date;
+                        }
+                    }
+                }
+                
                 Membership::create(array(
                     'area_id' => $obj->area_id,
                     'legislative_period_id' => $obj->legislative_period_id,
@@ -40,12 +58,25 @@ class MembershipsTableSeeder extends Seeder
                     'organization_id' => $obj->organization_id,
                     'person_id' => $obj->person_id,
                     'role' => $obj->role,
-                    'start_date' => $obj->start_date
+                    'start_date' => $obj->start_date,
+                    'end_date' => $obj->end_date
                 ));
             }
 
             // Start date does not exist
             else if(!isset($obj->start_date) && isset($obj->end_date)){
+                $obj->end_date = NULL;
+                // When end date does not exist get end date from json events file
+                // join event id with legislative_period_id and if exist get en date
+                // If the legislative period has not ended end_date = null
+                foreach($events as $ev){
+                    if($ev->classification == "legislative period" && $ev->id == $obj->legislative_period_id){
+                        if(isset($ev->start_date)){
+                            $obj->start_date = $ev->start_date;
+                        }
+                    }
+                }
+
                 Membership::create(array(
                     'area_id' => $obj->area_id,
                     'legislative_period_id' => $obj->legislative_period_id,
@@ -53,24 +84,41 @@ class MembershipsTableSeeder extends Seeder
                     'organization_id' => $obj->organization_id,
                     'person_id' => $obj->person_id,
                     'role' => $obj->role,
+                    'start_date' => $obj->start_date,
                     'end_date' => $obj->end_date
                 ));
             }
 
             // Start date and End date does not exist
+            // Get start date and end date from legislative period
             else if(!isset($obj->start_date) && !isset($obj->end_date)){
+                $obj->start_date = NULL;
+                $obj->end_date = NULL;
+                foreach($events as $ev){
+                    if($ev->classification == "legislative period" && $ev->id == $obj->legislative_period_id){
+                        if(isset($ev->start_date)){
+                            $obj->start_date = $ev->start_date;
+                        }
+                        if(isset($ev->end_date)){
+                            $obj->end_date = $ev->end_date;
+                        }
+                    }
+                }
+
                 Membership::create(array(
                     'area_id' => $obj->area_id,
                     'legislative_period_id' => $obj->legislative_period_id,
                     'on_behalf_of_id' => $obj->on_behalf_of_id,
                     'organization_id' => $obj->organization_id,
                     'person_id' => $obj->person_id,
-                    'role' => $obj->role
+                    'role' => $obj->role,
+                    'start_date' => $obj->start_date,
+                    'end_date' => $obj->end_date
                 ));
             }
             
             $counter++;
-            echo $counter . " " . $obj->person_id . " -> inserted" . PHP_EOL;
+            echo $counter . ": OK" . PHP_EOL;
         }
     }
 }
