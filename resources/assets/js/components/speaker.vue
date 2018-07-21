1f0632eb4e4bf60b69274b76a1089a84bf45f49a
Spirakos <uberi12345@hotmail.com>
2018-07-21 12:05:52 +0300

    changes for speaker

--- resources/assets/js/components/speaker.vue
+++ resources/assets/js/components/speaker.vue
@@ -1,52 +1,72 @@
 <template>
     <div class="container">
         <div v-if="ajaxData" class="row">
-           <div class="col-12 col-sm-3 col-md-3 col-lg-3 mt-2">
+           <div class="col-12 col-sm-4 col-md-4 col-lg-3 mt-2">
                <img :src="path + '/' + ajaxData.speakerData.image " alt="" class="img-fluid" style="width:50%;">
+                <!-- <table class="table mt-2 speaker-info" v-if="ajaxData.speakerData">
+                    <thead>
+                        
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Email</td>
+                            <td>{{ajaxData.speakerData.email}}</td>
+                        </tr>
+                        <tr>
+                            <td>Names</td>
+                            <td style="padding-right:0;">
+                                {{ajaxData.speakerData.greek_name}} 
+                                <span v-if="ajaxData.speakerData.greek_name != '' && ajaxData.speakerData.english_name != '' ">/</span> 
+                                {{ajaxData.speakerData.english_name}}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Wiki EL</td>
+                            <td><a :href="ajaxData.speakerData.wiki_el">wiki_el</a></td>
+                        </tr>
+                        <tr>
+                            <td>Wiki EN</td>
+                            <td><a :href="ajaxData.speakerData.wiki_en">wiki_en</a></td>
+                        </tr>
+                        <tr>
+                            <td>Twitter</td>
+                            <td><a :href="ajaxData.speakerData.twitter">twitter</a></td>
+                        </tr>
+                    </tbody>
+                </table> -->
+                <div v-if="ajaxData.speakerData">
+                    <div>
+                        {{ajaxData.speakerData.greek_name}} 
+                        <span v-if="ajaxData.speakerData.greek_name != '' && ajaxData.speakerData.english_name != '' ">/</span> 
+                        {{ajaxData.speakerData.english_name}}
+                    </div>
+                    <div>
+                        <span>{{ajaxData.speakerData.email}}</span>
+                    </div>
+                    <div>
+                        <a :href="ajaxData.speakerData.wiki_el">wiki_el</a>
+                    </div>
+                    <div>
+                        <a :href="ajaxData.speakerData.wiki_en">wiki_en</a>
+                    </div>
+                    <div>
+                        <a :href="ajaxData.speakerData.twitter">Twitter</a>
+                    </div>
+                </div>
            </div>
-           <div class="col-12 col-sm-9 col-md-9 col-lg-9 mt-2 p-0">
+           <div class="col-12 col-sm-8 col-md-8 col-lg-9 mt-2 p-0">
                <nav>
                     <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                        <a @click="currentTab = 'Information' " class="nav-item nav-link active" id="nav-information-tab" data-toggle="tab" href="#nav-information" role="tab" aria-controls="nav-information" aria-selected="true">Information</a>
+                        <!-- <a @click="currentTab = 'Information' " class="nav-item nav-link active" id="nav-information-tab" data-toggle="tab" href="#nav-information" role="tab" aria-controls="nav-information" aria-selected="true">Information</a> -->
+                        <a @click="currentTab = 'Speeches' " class="nav-item nav-link active" id="nav-speeches-tab" data-toggle="tab" href="#nav-speeches" role="tab" aria-controls="nav-speeches" aria-selected="true">Speeches</a>
                         <a @click="currentTab = 'Membership' " class="nav-item nav-link" id="nav-membership-tab" data-toggle="tab" href="#nav-membership" role="tab" aria-controls="nav-membership" aria-selected="false">Membership</a>
-                        <a @click="currentTab = 'Speeches' " class="nav-item nav-link" id="nav-speeches-tab" data-toggle="tab" href="#nav-speeches" role="tab" aria-controls="nav-speeches" aria-selected="false">Speeches</a>
                     </div>
                 </nav>
                 <div class="tab-content" id="nav-tabContent">
-                    <div class="tab-pane fade show active" id="nav-information" role="tabpanel" aria-labelledby="nav-information-tab">
-                        <table class="table mt-2 speaker-info" v-if="ajaxData.speakerData">
-                            <thead>
-                                
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Email</td>
-                                    <td>{{ajaxData.speakerData.email}}</td>
-                                </tr>
-                                <tr>
-                                    <td>Names</td>
-                                    <td style="padding-right:0;">
-                                        {{ajaxData.speakerData.greek_name}} 
-                                        <span v-if="ajaxData.speakerData.greek_name != '' && ajaxData.speakerData.english_name != '' ">/</span> 
-                                        {{ajaxData.speakerData.english_name}}</td>
-                                </tr>
-                                <tr>
-                                    <td>Wiki EL</td>
-                                    <td><a :href="ajaxData.speakerData.wiki_el">wiki_el</a></td>
-                                </tr>
-                                <tr>
-                                    <td>Wiki EN</td>
-                                    <td><a :href="ajaxData.speakerData.wiki_en">wiki_en</a></td>
-                                </tr>
-                                <tr>
-                                    <td>Twitter</td>
-                                    <td><a :href="ajaxData.speakerData.twitter">twitter</a></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="tab-pane fade" id="nav-membership" role="tabpanel" aria-labelledby="nav-membership-tab">{{currentTab}}</div>
-                    <div class="tab-pane fade speeches-container" id="nav-speeches" role="tabpanel" aria-labelledby="nav-speeches-tab">
+                    <!-- <div class="tab-pane fade show active" id="nav-information" role="tabpanel" aria-labelledby="nav-information-tab">
+                        
+                    </div> -->
+                    <div class="tab-pane fade show speeches-container active" id="nav-speeches" role="tabpanel" aria-labelledby="nav-speeches-tab">
                         <div v-for="speech in ajaxData.speechesData" :key="speech.speech_id" class="speeches">
                             <!-- <blockquote class="blockquote">
                                 <p>{{speech.speech}}</p>
@@ -62,11 +82,12 @@
                                     <mark>{{speech.speech_conference_date}}</mark>
                                 </small>
                             </h5>
-                            <div>
+                            <div class="text-left">
                                 {{speech.speech}}
                             </div>
                         </div>
                     </div>
+                    <div class="tab-pane fade" id="nav-membership" role="tabpanel" aria-labelledby="nav-membership-tab">{{currentTab}}</div>
                 </div>
            </div>
         </div>
