46a0f15d07efdfeb2f62f35e19e69de62c7e0c01
spiros_kraken <uberi12345@hotmail.com>
2018-08-18 14:24:32 +0300

    Auto stash before merge of "develop_final" and "origin/develop_final"
    change welcome page

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <img src="/img/greek-parliament.jpg">
+            <!-- <img src="/img/greek-parliament.jpg"> -->
+            <h2>Welcome</h2>
+            <br>
+            <p>greekparliament.info is a platform for the presentation of the greek parliament transcripts</p>
         </div>
     </div>
 </template>
@@ -9,7 +12,7 @@
 <script>
     export default {
         mounted() {
-            console.log('Component mounted.')
+            //console.log('Component mounted.')
         }
     }
 </script>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -63,7 +63,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn" style="background-color: #17a2b8;">
+                                <button type="submit" class="btn" style="background-color: #17a2b8;color:white;">
                                     {{ __('Register') }}
                                 </button>
                             </div>
