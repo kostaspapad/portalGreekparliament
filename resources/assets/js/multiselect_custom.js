42f35dfd5b973c2b912419a83377378d90a32e52
spiros_kraken <uberi12345@hotmail.com>
2018-08-26 16:28:45 +0300

    custom vue multiselect plugin

---
+++ resources/assets/js/multiselect_custom.js
@@ -0,0 +1,7 @@
+import multiselectCustom from './multiselect/multiselectCustom'
+import multiselectMixin from './multiselect/multiselectCustomMixin'
+import pointerMixin from './multiselect/multiselectCustomPointerMixin'
+
+export default multiselectCustom
+
+export { multiselectCustom, multiselectMixin, pointerMixin }
