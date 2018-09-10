adfff0a6ced678402075f0894fc8f1ddcf903ed0
kospa <kospa@protonmai.com>
2018-09-10 14:35:29 +0300

    Update speaker.vue

--- resources/assets/js/components/speaker.vue
+++ resources/assets/js/components/speaker.vue
@@ -1,121 +1,124 @@
 <template>
     <div class="container">
-        <div v-if="ajaxData" class="row">
-           <div class="col-12 col-sm-4 col-md-4 col-lg-3 mt-2">
-               <img :src="path + '/' + ajaxData.speakerData.image " alt="" class="img-fluid" style="width:50%;">
-                <!-- <table class="table mt-2 speaker-info" v-if="ajaxData.speakerData">
-                    <thead>
-                        
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Email</td>
-                            <td>{{ajaxData.speakerData.email}}</td>
-                        </tr>
-                        <tr>
-                            <td>Names</td>
-                            <td style="padding-right:0;">
-                                {{ajaxData.speakerData.greek_name}} 
-                                <span v-if="ajaxData.speakerData.greek_name != '' && ajaxData.speakerData.english_name != '' ">/</span> 
-                                {{ajaxData.speakerData.english_name}}
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Wiki EL</td>
-                            <td><a :href="ajaxData.speakerData.wiki_el">wiki_el</a></td>
-                        </tr>
-                        <tr>
-                            <td>Wiki EN</td>
-                            <td><a :href="ajaxData.speakerData.wiki_en">wiki_en</a></td>
-                        </tr>
-                        <tr>
-                            <td>Twitter</td>
-                            <td><a :href="ajaxData.speakerData.twitter">twitter</a></td>
-                        </tr>
-                    </tbody>
-                </table> --> 
-                <div v-if="ajaxData.speakerData" class="mt-2">
-                    <div>
-                        {{ajaxData.speakerData.greek_name}} 
-                        <span v-if="ajaxData.speakerData.greek_name != '' && ajaxData.speakerData.english_name != '' ">/</span> 
-                        {{ajaxData.speakerData.english_name}}
-                    </div>
-                    <div>
-                        <span>{{ajaxData.speakerData.email}}</span>
-                    </div>
-                    <div>
-                        <a :href="ajaxData.speakerData.wiki_el">wiki_el</a>
-                    </div>
-                    <div>
-                        <a :href="ajaxData.speakerData.wiki_en">wiki_en</a>
-                    </div>
-                    <div>
-                        <a :href="ajaxData.speakerData.twitter">Twitter</a>
-                    </div>
-                </div>
-           </div>
-           <div class="col-12 col-sm-8 col-md-8 col-lg-9 mt-2 p-0">
-               <nav>
-                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                        <!-- <a @click="currentTab = 'Information' " class="nav-item nav-link active" id="nav-information-tab" data-toggle="tab" href="#nav-information" role="tab" aria-controls="nav-information" aria-selected="true">Information</a> -->
-                        <a @click="currentTab = 'Speeches' " class="nav-item nav-link active" id="nav-speeches-tab" data-toggle="tab" href="#nav-speeches" role="tab" aria-controls="nav-speeches" aria-selected="true">Speeches</a>
-                        <a @click="currentTab = 'Membership' " class="nav-item nav-link" id="nav-membership-tab" data-toggle="tab" href="#nav-membership" role="tab" aria-controls="nav-membership" aria-selected="false">Membership</a>
-                    </div>
-                </nav>
-                <div class="tab-content" id="nav-tabContent">
-                    <!-- <div class="tab-pane fade show active" id="nav-information" role="tabpanel" aria-labelledby="nav-information-tab">
-                        
-                    </div> -->
-                    <div class="tab-pane fade show speeches-container active" id="nav-speeches" role="tabpanel" aria-labelledby="nav-speeches-tab">
-                        <div v-for="speech in ajaxData.speechesData" :key="speech.speech_id" class="speeches">
-                            <!-- <blockquote class="blockquote">
-                                <p>{{speech.speech}}</p>
-                                <footer class="blockquote-footer"><small>{{speech.speech_conference_date}}</small></footer>
-                            </blockquote> -->
-                            <!-- <h5 class="mt-2" v-if="showDate">
-                                <small>
-                                    <mark  v-if="checkDate(speech.speech_conference_date)" style="background-color: #17a2b840;">{{conferenceDate}}</mark>
-                                </small>
-                            </h5> -->
-                            <h5 class="mt-2">
-                                <small>
-                                    <mark>{{speech.speech_conference_date}}</mark>
-                                </small>
-                            </h5>
-                            <div class="text-left">
-                                {{speech.speech}}
-                            </div>
+        <div class="speaker-container">
+            <div v-if="ajaxDoneSpeaker" class="row">
+                <div class="col-12 col-sm-4 col-md-4 col-lg-3 mt-2">
+                    <img :src="path + '/' + ajaxData.speakerData.data.data.image " alt="" class="img-fluid" style="width:50%;">
+                    <div v-if="ajaxData.speakerData" class="mt-2">
+                        <div>
+                            {{ajaxData.speakerData.data.data.greek_name}}
+                            <span v-if="ajaxData.speakerData.data.data.greek_name != '' && ajaxData.speakerData.data.data.english_name != '' ">/</span>
+                            {{ajaxData.speakerData.data.data.english_name}}
+                        </div>
+                        <div>
+                            <span>{{ajaxData.speakerData.data.data.email}}</span>
+                        </div>
+                        <div>
+                            <a :href="ajaxData.speakerData.data.data.wiki_el">wiki_el</a>
+                        </div>
+                        <div>
+                            <a :href="ajaxData.speakerData.data.data.wiki_en">wiki_en</a>
+                        </div>
+                        <div>
+                            <a :href="ajaxData.speakerData.data.data.twitter">Twitter</a>
                         </div>
                     </div>
-                    <div class="tab-pane fade" id="nav-membership" role="tabpanel" aria-labelledby="nav-membership-tab">{{currentTab}}</div>
                 </div>
-           </div>
-        </div>
-        <div v-else>
-            <img :src="path + '/Spinner.gif' "/>
+                <div class="col-12 col-sm-8 col-md-8 col-lg-9 mt-2">
+                    <nav>
+                        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                            <!-- <a @click="currentTab = 'Information' " class="nav-item nav-link active" id="nav-information-tab" data-toggle="tab" href="#nav-information" role="tab" aria-controls="nav-information" aria-selected="true">Information</a> -->
+                            <a @click="currentTab = 'Speeches' " class="nav-item nav-link active" id="nav-speeches-tab"
+                                data-toggle="tab" href="#nav-speeches" role="tab" aria-controls="nav-speeches"
+                                aria-selected="true">Speeches</a>
+                            <a @click="currentTab = 'Membership' " class="nav-item nav-link" id="nav-membership-tab"
+                                data-toggle="tab" href="#nav-membership" role="tab" aria-controls="nav-membership"
+                                aria-selected="false">Membership</a>
+                        </div>
+                    </nav>
+                    <div class="tab-content py-3" id="nav-tabContent">
+                        <!-- <div class="tab-pane fade show active" id="nav-information" role="tabpanel" aria-labelledby="nav-information-tab">
+                        
+                    </div> -->
+                        <div class="tab-pane fade show speeches-container active" id="nav-speeches" role="tabpanel"
+                            aria-labelledby="nav-speeches-tab">
+                            <div v-if="ajaxDoneSpeeches">
+                                <div v-for="speech in ajaxData.speechesData.data.data" :key="speech.speech_id" class="speeches py-2">
+                                    <div class="row">
+                                        <small><a :href="`/speaker/${speech.greek_name}`" class="text-info">{{speech.greek_name}}</a> · {{speech.speech_conference_date}}</small>
+                                    </div>
+                                    <div class="">
+                                        <p class="text-left">{{speech.speech}}</p>
+                                    </div>
+                                </div>
+                                <div class="col-12 mt-5" style="padding-left: 2.5rem;">
+                                    <pagination :data="ajaxData.speechesData.data.meta" @pagination-change-page="changePage" :limit=1>
+                                        <span slot="prev-nav">&lt;</span>
+                                        <span slot="next-nav">&gt;</span>
+                                    </pagination>
+                                </div>
+                            </div>
+                            <div v-else>
+                                <h5>No speeches found</h5>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="nav-membership" role="tabpanel" aria-labelledby="nav-membership-tab">{{currentTab}}</div>
+                    </div>
+                </div>
+            </div>
+            <div v-else>
+                <img :src="path + '/Spinner.gif' " />
+            </div>
         </div>
     </div>
 </template>
 
-<style scoped>
-    .nav-tabs a{
+<style lang="scss" scoped>
+    $ContainerColor: white;
+
+    .speaker-container {
+        background-color: $ContainerColor;
+        border-radius: 5px;
+    }
+
+    .nav-tabs a {
         color: #17a2b8;
     }
-    .speeches-container{
-        height: 630px;
-        overflow-y: scroll;
+
+    .speeches {
+        border-radius: 3px;   
     }
-    .speeches-container .speeches:not(:last-child){
+    // .speeches-container {
+    //     height: 630px;
+    //     overflow-y: scroll;
+    // }
+
+    /* .speeches-container .speeches:not(:last-child){
         border-bottom: 1px dashed #17a2b8;
-    }
+    }*/
     /* .speeches:last-child{
         border-bottom: none;
     } */
-    .speaker-info td{
+    .speaker-info td {
         border: 0;
     }
-    @media (max-width: 352px) { 
-        nav{
+    .sort-ul{
+        font-size: 0.9em;
+        color: #6c6b68;
+        margin-left: -2.3em;
+        margin-bottom: 0;
+    }
+    .sort-ul li{
+        display: inline-block;
+        margin-left: 0.5em;
+    }
+    .sort-text{
+        color: inherit;
+        text-decoration: underline;
+        cursor: pointer;
+    }
+    @media (max-width: 352px) {
+        nav {
             font-size: 0.78rem;
         }
     }
@@ -127,7 +130,7 @@
             name: String,
             path: String
         },
-        data(){
+        data() {
             return {
                 ajaxData: {
                     speechesData: [],
@@ -137,10 +140,49 @@
                 conferenceDate: null,
                 showDate: true,
                 currentTab: 'Information',
-                defaultImg: 'default_speaker_icon.png'
+                defaultImg: 'default_speaker_icon.png',
+                ajaxDoneSpeaker: false,
+                ajaxDoneSpeeches: false,
+                noDataSpeaker: true,
+                noDataSpeeches: true,
+                loading: true,
+                order_field: 'conference_date',
+                order_orientation: 'asc'
             }
         },
-        methods:{
+        methods: {
+            sortBy(sortField, sortOrientation = null){
+                if(sortField){
+                    this.order_field = sortField
+                }
+                if(sortOrientation == 'asc'){
+                    this.order_orientation = 'desc'
+                }else{
+                    this.order_orientation = 'asc'
+                }
+                if(this.order_field && this.order_orientation){
+                    this.getSpeakers()
+                }
+            },
+            changePage(page){
+                //for pagination
+                var self = this
+                let url = null
+
+                if (this.finalName) {
+                    url = '/api/v1/speeches/speaker/name/' + this.finalName + '?page=' + page
+                }
+                
+                axios.get(this.$root.host + url)
+                .then(function(response){
+                    if(response.status == 200 && response.statusText == "OK"){
+                        self.ajaxData.speechesData = response
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error)
+                });
+            },
             IsJsonString(str) {
                 var json;
                 try {
@@ -150,57 +192,71 @@
                 }
                 return json;
             },
-            printImg(img){
-                if(img == 'default-speaker.jpg' || img == null){
+            printImg(img) {
+                if (img == 'default-speaker.jpg' || img == null) {
                     return this.defaultImg;
-                }else{
+                } else {
                     return img;
                 }
             },
-            checkDate(conf_date){
-                if(this.conferenceDate == null){
+            checkDate(conf_date) {
+                if (this.conferenceDate == null) {
                     this.conferenceDate = conf_date
-                }else{
-                    if(this.conferenceDate == conf_date){
+                } else {
+                    if (this.conferenceDate == conf_date) {
                         this.showDate = false
-                    }else{
+                    } else {
                         this.conferenceDate = conf_date
                         this.showDate = true
                     }
                 }
             },
-            getSpeakerSpeeches(){
-                var self = this;
-                axios.get(this.$root.host+'/api/v1/speeches/speaker/name/'+this.finalName)
-                .then(function(response){
-                    console.log(response)
-                    if(response.status == 200 && response.data.data){
-                        self.ajaxData.speechesData = response.data.data
-                    }
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
+            getSpeakerSpeeches() {
+                const self = this;
+
+                setTimeout( () => {
+                    axios.get(this.$root.host + '/api/v1/speeches/speaker/name/' + this.finalName)
+                    .then(function (response) {
+                        if (response.status == 200 && response) {
+                            self.noDataSpeeches = false
+                            self.ajaxData.speechesData = response
+                        } else {
+                            self.noDataSpeeches = true
+                        }
+                        self.ajaxDoneSpeeches = true 
+
+                    }).catch(function (error) {
+                        console.log(error);
+                    });
+
+                }, 1000)
             },
-            getSpeakerData(){
-                var self = this
-                axios.get(this.$root.host+'/api/v1/speaker/name/'+this.finalName)
-                .then(function(response){
-                    console.log(response)
-                    if(response.status == 200 && response.data.data){
-                        self.ajaxData.speakerData = response.data.data
-                    }
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
+            getSpeakerData() {
+                const self = this
+
+                setTimeout( () => {
+                    axios.get(this.$root.host + '/api/v1/speaker/name/' + this.finalName)
+                    .then(function (response) {
+                        if (response.status == 200 && response) {
+                            self.noDataSpeaker = false
+                            self.ajaxData.speakerData = response
+                        } else {
+                            self.noDataSpeaker = true
+                        }
+                        self.ajaxDoneSpeaker = true
+
+                    }).catch(function (error) {
+                        console.log(error);
+                    });
+
+                } ,1000)
             }
         },
-        computed:{
-            
+        computed: {
+
         },
         created() {
-            console.log(decodeURIComponent(this.name))
+            this.loading = false
             this.finalName = decodeURIComponent(this.name)
             this.finalName = this.finalName.replace(/\+/g, " ")
             console.log(this.finalName)
