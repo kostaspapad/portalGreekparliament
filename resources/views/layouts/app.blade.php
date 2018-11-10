e04d3d9aabe89e7a7dc5b09145e0008ff0b65361
kospa <kospa@protonmai.com>
2018-11-10 17:49:26 +0200

    [Frontend] Fix css import for font-awesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,6 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-58724981-1"></script>
         <script>
@@ -12,27 +11,20 @@
             gtag('config', 'UA-58724981-1');
         </script>
         <script src="https://www.gstatic.com/charts/loader.js"></script> -->
-        
         <script src="https://www.gstatic.com/charts/loader.js"></script>
-
+        <meta name="theme-color" content="#33a8ff">
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="description" content="Online presentation of the Greek Parliament transcripts" />
-    
         <title>Greekparliament.info</title>
-
-        <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
-        
-        <!-- CSS -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
             <vue-layout></vue-layout>
         </div>
- 
     </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,7 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+mix.setPublicPath('public');
+mix.setResourceRoot('../');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
