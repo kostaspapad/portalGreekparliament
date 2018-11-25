8b76d067e2b20fbbe985495304d7ebee7f242bcf
Fotis <fparas11@gmail.com>
2018-11-25 22:36:27 +0100

    Front page

--- resources/assets/js/views/Home.vue
+++ resources/assets/js/views/Home.vue
@@ -2,16 +2,22 @@
     <div class="container-imagebox" style="margin-top: -2rem;">
         <div class="row" style="margin-right: 0;">
             <div class="image-container col-12 col-sm-6 col-md-4" style="    padding-left: 20px;">
-                <img src="/img/justice2.jpg" class="img-fluid">
+                <img src="/img/mainpage_logo.png" class="img-fluid">
                 <div>
-                    <h3 class="image-text">Find a speaker</h3>
+                    <h3 class="image-text">Αναζήτηση ομιλητή</h3>
                     <search-plugin></search-plugin>
                 </div>
             </div>
             <div class="imagebox-text col-12 col-sm-6 col-md-8">
-                <h2 class="imagebox-text-title">Democracy: it’s for everyone</h2>
+                <h2 class="imagebox-text-title">Μια προσπάθεια εξόρυξης και ταξινόμησης δεδομένων απο τα πρακτικά ομιλιών του Ελληνικού Κοινοβουλίου.</h2>
+                <h3 class="imagebox-text-subtitle">Alpha Version</h3>
                 <div class="imagebox-text">
-                    <p>You shouldn’t have to be an expert to understand what goes on in Parliament. Your politicians represent you… but what exactly do they do in your name?</p>
+                    <li>5.058 συνεδριάσεις (3 Ιουλίου 1989 μέχρι σήμερα).</li>
+                    <li>1.184.079 ομιλίες.</li>
+                    <li>1.786 ομιλητές.</li>
+                    <li>26 κόμματα.</li>
+
+
                 </div>
             </div>
         </div>
