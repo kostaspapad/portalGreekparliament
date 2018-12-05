2b8c39587fd195e01da25853c7ec37801a764e75
spiros <uberi12345@hotmail.com>
2018-12-05 22:23:57 +0200

    [Back-end] Remove unnecessary properties from Speech Resource

--- app/Http/Resources/Speech.php
+++ app/Http/Resources/Speech.php
@@ -14,26 +14,28 @@ class Speech extends JsonResource
      */
     public function toArray($request)
     {
+        if (!isset($this->isFavorite)){
+            $this->isFavorite = NULL;
+        }
         // return parent::toArray($request);
         $arr = [
             'speech_id' => $this->speech_id,
             'speech_conference_date' => $this->speech_conference_date,
             'speaker_id' => $this->speaker_id,
             'speech' => $this->speech,
-            'f_name' => $this->f_name,
-            'party_id' => $this->party_id,
+            // 'f_name' => $this->f_name,
             // 'created_at' => $this->created_at,
             // 'updated_at' => $this->updated_at,
-            'md5' => $this->md5,
+            // 'md5' => $this->md5,
             'speaker_id' => $this->speaker_id,
             'english_name' => $this->english_name,
             'greek_name' => $this->greek_name,
             'image' => $this->image,
-            'email' => $this->email,
-            'wiki_el' => $this->wiki_el,
-            'wiki_en' => $this->wiki_en,
-            'twitter' => $this->twitter,
-            'website' => $this->website,
+            //'email' => $this->email,
+            // 'wiki_el' => $this->wiki_el,
+            // 'wiki_en' => $this->wiki_en,
+            // 'twitter' => $this->twitter,
+            // 'website' => $this->website,
             'on_behalf_of_id' => $this->on_behalf_of_id,
             'fullname_el' => $this->fullname_el,
             'party_color' => $this->color,
@@ -43,6 +45,7 @@ class Speech extends JsonResource
         
         // array_filter without second argument removes null elements of entry array
         return array_filter((array) $arr);
+        
     }
 
     /**
