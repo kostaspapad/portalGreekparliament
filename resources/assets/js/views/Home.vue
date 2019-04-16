906104f5dd5437825131b2c0ba42a3d6892576fc
kospa <kospa@protonmai.com>
2019-04-16 21:44:51 +0300

    [Frontend] Change home page, about page

--- resources/assets/js/views/Home.vue
+++ resources/assets/js/views/Home.vue
@@ -10,23 +10,67 @@
             </div>
             <div class="imagebox-text col-12 col-sm-6 col-md-8">
                 <h3 class="imagebox-text-title">Μια προσπάθεια εξόρυξης και ταξινόμησης δεδομένων απο τα πρακτικά ομιλιών του Ελληνικού Κοινοβουλίου.</h3>
-                <h3 class="imagebox-text-subtitle d-block d-sm-none text-center">Alpha Version</h3>
-                <h3 class="imagebox-text-subtitle d-none d-sm-block">Alpha Version</h3>
+                <h3 class="imagebox-text-subtitle d-none d-sm-block">Δοκιμαστική έκδοση</h3>
                 <div class="imagebox-text">
-                    <ul>
+                    <p>Βρείτε πληροφορίες για βουλευτές, κομματικές παρατάξεις και συνεδριάσεις</p>
+                    <p>Αναζητήστε φράσεις και λέξεις κλειδιά σε όλα τα πρακτικά</p>
+                    <!-- <p>Δημιουργίστε λίστες με λέξεις στις οποίες θέλετε να ενημερώνεστε όταν αναφέρονται σε κάποια συνεδρίαση</p> -->
+                    <!-- <ul>
                         <li>5.058 συνεδριάσεις (3 Ιουλίου 1989 μέχρι σήμερα).</li>
                         <li>1.184.079 ομιλίες.</li>
                         <li>1.786 ομιλητές.</li>
                         <li>26 κόμματα.</li>
-                    </ul>
+                    </ul> -->
                 </div>
             </div>
         </div>
+        <!-- <div v-for="ajaxData.speakers">
+            <timeline 
+                :data="ajaxData.timelineData" 
+                :colors="ajaxData.memberships.party_colors"
+            ></timeline>
+        </div> -->
     </div> 
 </template>
 
 <script>
+import { mapState, mapGetters } from 'vuex'
 export default {
-
+    data() {
+        return {
+            ajaxData: {
+                speakers: [
+                    
+                ]
+            }
+        }
+    },
+    
+    // timelineData: []
+    methods: {
+        getTimelineMembershipData() {
+            
+            setTimeout(() => {
+                api.call('get', this.api_path + 'speakers/timeline')
+                .then( response => {
+                    
+                    if(response.status == 200 && response.data){
+                        response.data.forEach( jsonEntry => {
+                            console.log(jsonEntry[0])
+                        
+                        })
+                    }
+                })
+            }, 500)
+        }
+    },
+    computed: {
+        ...mapGetters({
+            api_path: 'get_api_path'
+        })
+    },
+    created() {
+        // this.getTimelineMembershipData()
+    }
 }
 </script>
